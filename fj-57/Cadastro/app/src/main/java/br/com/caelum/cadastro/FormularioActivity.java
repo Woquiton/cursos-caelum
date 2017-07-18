f22bd6f4b3d8b73a9a1f95eb32875baf5782e9bc
Carlos Dias <carlospdiase@gmail.com>
2017-07-18 11:58:33 -0300

    menu

--- fj-57/Cadastro/app/src/main/java/br/com/caelum/cadastro/FormularioActivity.java
+++ fj-57/Cadastro/app/src/main/java/br/com/caelum/cadastro/FormularioActivity.java
@@ -2,6 +2,8 @@ package br.com.caelum.cadastro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,5 +27,25 @@ public class FormularioActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok: {
+                Toast.makeText(this, "ok", Toast.LENGTH_LONG).show();
+                finish();
+                return false;
+            }
+            default: return super.onOptionsItemSelected(item);
+        }
+
     }
 }
